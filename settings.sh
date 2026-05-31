671d81f6525dfc26ac9274a469443bb925c2db67
AlanShadow931 <alanshadow931@gmail.com>
2026-05-31 21:19:44 +0800

    新增設定訊息

--- settings.sh
+++ settings.sh
@@ -6,18 +6,21 @@ echo "Setting up system configurations..."
 # 時區與時間 (Timezone & Time)
 ln -sf /usr/share/zoneinfo/Asia/Taipei /etc/localtime
 hwclock --systohc
+echo "Time zone set to Asia/Taipei and hardware clock synchronized."
 
 # 語言與區域 (Locale & Language)
 echo "en_US.UTF-8 UTF-8" > /etc/locale.gen
 echo "zh_TW.UTF-8 UTF-8" >> /etc/locale.gen
 locale-gen
 echo "LANG=zh_TW.UTF-8" > /etc/locale.conf
+echo "Langeuage set to zh_TW.UTF-8."
 
 # 主機名稱與 hosts 設定 (Hostname & Hosts)
 echo "ArchLinux" >> /etc/hostname
 echo "127.0.0.1 localhost" >> /etc/hosts
 echo "::1 localhost" >> /etc/hosts
 echo "127.0.1.1 ArchLinux" >> /etc/hosts
+echo "Hostname and hosts configured."
 
 # 使用者與權限 (User & Permissions)
 echo "Changing root password..."
@@ -25,13 +28,16 @@ passwd
 useradd -m -G wheel,docker -s /bin/bash $USERNAME
 echo "Changing password for $USERNAME..."
 passwd $USERNAME
+echo "User $USERNAME created and added to wheel and docker groups."
 
 # 設定 sudo 權限
 echo "$USERNAME ALL=(ALL:ALL) ALL" >> /etc/sudoers
+echo "Sudo permissions granted to $USERNAME."
 
 git clone https://github.com/ZWolken/PingFang.git /tmp/PingFang
 mkdir -p /usr/share/fonts/PingFang
 cp /tmp/PingFang/*.ttf /usr/share/fonts/PingFang/
+echo "PingFang fonts installed."
 
 # GRUB 引導程式安裝與設定 (GRUB Bootloader)
 grub-install --target=x86_64-efi --bootloader-id=GRUB --efi-directory=/boot
